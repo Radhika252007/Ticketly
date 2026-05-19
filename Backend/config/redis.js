97d270421f8594d7b123e10547865e1679010cdf
Radhika <mukeshmadaan2025@gmail.com>
2026-05-19 16:32:47 +0530

    Added Redis Changes

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -1,18 +1,19 @@
 import Redis from 'ioredis';
 
-const client = new Redis({
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  port: process.env.REDIS_PORT || 6379,
-  maxRetriesPerRequest: 3
+const client = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null
 });
 
-client.on('connect', () => console.log('Connected to Redis!'));
-client.on('error', (err) => console.error('Redis error:', err));
+client.on('connect', () => {
+  console.log('Connected to Redis!');
+});
 
-export const bullConnection = {
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
+client.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
+export const bullConnection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null
-};
+});
 
 export default client;
